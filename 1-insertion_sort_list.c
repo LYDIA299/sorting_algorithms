077db8f30fc91ac0d496e29cd10473fba9494b10
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-30 23:34:39 +0100

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 
+void swap_nodes(listint_t *cur_node, listint_t *prev_node, listint_t **head);
 /**
  * insertion_sort - sorts a doubly linked list od integers in ascending order
  * @list: list to sort
@@ -7,38 +8,53 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *cur_node = NULL, *prev_node = NULL, *tmp_node = NULL;
+	listint_t *cur_node = NULL, *prev_node = NULL;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	cur_node = (*list)->next;
 	prev_node = cur_node->prev;
+
 	while (cur_node != NULL)
 	{
-		tmp_node = cur_node;
+		prev_node = cur_node->prev;
+		while (prev_node != NULL && prev_node->n > cur_node->n)
+		{
+			swap_nodes(cur_node, prev_node, list);
+			prev_node = cur_node->prev;
+		}
 		cur_node = cur_node->next;
-		prev_node->next = cur_node;
-		cur_node->prev = prev_node;
-		while (prev_node != NULL)
-		{
-			if (tmp_node->n > prev_node->n)
-			{
-				/* swap time */
-				tmp_node->next = prev_node->next;
-				tmp_node->prev = prev_node;
-				prev_node->next->prev = tmp_node;
-				prev_node->next = tmp_node;
-				print_list(*list);
-				break;
-			}
-			prev_node = prev_node->prev;
-		}
-		if (prev_node == NULL)
-		{
-			tmp_node->next = *list;
-			tmp_node->prev = prev_node;
-			(*list)->prev = tmp_node;
-		}
 	}
+}
+
+/**
+ * swap_nodes - swaps the nodes
+ * @cur_nose: less node
+ * @prev_node: higher node
+ * @head: head pointer
+ * Return: nothing
+ */
+void swap_nodes(listint_t *cur_node, listint_t *prev_node, listint_t **head)
+{
+	listint_t *after_node = cur_node->next;
+	listint_t *before_node = prev_node->prev;
+
+	if (after_node != NULL)
+	{
+		after_node->prev = prev_node;
+	}
+	if (before_node != NULL)
+	{
+		before_node->next = cur_node;
+	}
+	prev_node->next = after_node;
+	cur_node->prev = before_node;
+	cur_node->next = prev_node;
+	prev_node->prev = cur_node;
+
+	/* making sure that the head pointer is pointing to the first node */
+	if (*head == prev_node)
+		*head = cur_node;
+	print_list(*head);
 }
